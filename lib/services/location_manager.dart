43d3705461b1b794f2c1a3b4dbb0ff4d5de76bd5
mboti <botimounir@yahoo.fr>
2022-07-24 14:08:54 +0200

    5 - Location maneger

--- lib/services/location_manager.dart
+++ lib/services/location_manager.dart
@@ -1,3 +1,36 @@
+import 'package:geolocator/geolocator.dart';
+
 class LocationManager{
+
+  Future<Position> start() async{
+    final bool locationEnabled = await Geolocator.isLocationServiceEnabled();
+    return (locationEnabled) ? await handlePermission() : await errorString("Location désactivée");
+  }
+
+  Future<Position> handlePermission() async{
+    final LocationPermission locationPermission = await Geolocator.checkPermission();
+    return await resultPermission(locationPermission);
+  }
+
+  Future<Position> resultPermission(LocationPermission locationPermission) async{
+    switch (locationPermission){
+      case LocationPermission.deniedForever:
+        return errorString("Permission refusée");
+      case LocationPermission.denied:
+        return request().then((newPermission) => resultPermission(newPermission));
+      case LocationPermission.unableToDetermine:
+        return request().then((newPermission) => resultPermission(newPermission));
+      case LocationPermission.whileInUse:
+        return await getPosition();
+      case LocationPermission.always:
+        return await getPosition();
+    }
+  }
+
+  Future<Position> getPosition() async => await Geolocator.getCurrentPosition();
+
+  Future<LocationPermission> request() async => await Geolocator.requestPermission();
+
+  Future<Position> errorString(String err) async => await Future.error(err);
 
 }
